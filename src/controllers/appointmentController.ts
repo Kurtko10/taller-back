410e731117abbe8403ff92d80c701ef431bfef16
Kurtko10 <kurtkoaf@gmail.com>
2024-05-25 21:53:29 +0200

    Add: funciones editar una cita y eliminar cita

--- src/controllers/appointmentController.ts
+++ src/controllers/appointmentController.ts
@@ -10,7 +10,7 @@ import { UserCar } from "../models/UserCar";
 
 export const appointmentController = {
 
-
+// Ver todas las citas
 async getAllAppointments(req: Request, res: Response): Promise<void> {
     try {
         const appointments = await Appointment.find({
@@ -57,7 +57,6 @@ async getAllAppointments(req: Request, res: Response): Promise<void> {
     }
 },
 // Citas de un mecanico
-
 async getAppointmentsByWorkerId(req: Request, res: Response): Promise<void> {
     try {
         const userId = req.tokenData.userId;
@@ -118,7 +117,6 @@ async getAppointmentsByWorkerId(req: Request, res: Response): Promise<void> {
 },
 
 // Citas de un usuario
-
 async getAppointmentsByClientId(req: Request, res: Response): Promise<void> {
     try {
         const userId = req.tokenData.userRole === 'admin'|| req.tokenData.userRole === 'manager' && req.params.clientId
@@ -168,6 +166,60 @@ async getAppointmentsByClientId(req: Request, res: Response): Promise<void> {
         }));
 
         res.status(200).json(formattedAppointments);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+},
+// Obtener una cita por ID
+async getAppointmentById(req: Request, res: Response): Promise<void> {
+    try {
+        const appointmentId = Number(req.params.id);
+
+        if (isNaN(appointmentId)) {
+            res.status(400).json({ message: "Invalid appointment ID" });
+            return;
+        }
+
+        const appointment = await Appointment.findOne({
+            where: { id: appointmentId },
+            relations: ["service", "userClient", "userWorker", "car"]
+        });
+
+        if (!appointment) {
+            res.status(404).json({ message: "Appointment not found" });
+            return;
+        }
+
+        const formattedAppointment = {
+            id: appointment.id,
+            date: appointment.date,
+            status: appointment.status,
+            observations: appointment.observations,
+            service: appointment.service ? {
+                id: appointment.service.id,
+                name: appointment.service.name,
+            } : null,
+            worker: appointment.userWorker ? {
+                id: appointment.userWorker.id,
+                name: appointment.userWorker.firstName,
+                phone: appointment.userWorker.phone
+            } : null,
+            client: appointment.userClient ? {
+                id: appointment.userClient.id,
+                name: appointment.userClient.firstName,
+                phone: appointment.userClient.phone
+            } : null,
+            car: appointment.car ? {
+                id: appointment.car.id,
+                licensePlate: appointment.car.licensePlate,
+                carBrand: appointment.car.carBrand,
+                model: appointment.car.model,
+                year: appointment.car.year
+            } : null,
+        };
+
+        res.status(200).json(formattedAppointment);
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: "Internal server error" });
@@ -288,7 +340,150 @@ async  createAppointment(req: Request, res: Response): Promise<void> {
         console.error('Error in createAppointment:', error);
         res.status(500).json({ message: "Internal server error" });
     }
-}
+},
+// Editar una cita
+async updateAppointment(req: Request, res: Response): Promise<void> {
+    try {
+        const appointmentId = Number(req.params.id);
+        const { date, observations, status, service_id, user_id_client, user_id_worker, car_id } = req.body;
+
+        if (isNaN(appointmentId)) {
+            res.status(400).json({ message: "Invalid appointment ID" });
+            return;
+        }
+
+        const appointment = await Appointment.findOne({ 
+            where: { id: appointmentId },
+            relations: ["service", "userClient", "userWorker", "car"]
+        });
+
+        if (!appointment) {
+            res.status(404).json({ message: "Appointment not found" });
+            return;
+        }
+
+        if (date) {
+            const newDate = new Date(date);
+            if (isNaN(newDate.getTime())) {
+                res.status(400).json({ message: "Invalid date format" });
+                return;
+            }
+            appointment.date = newDate;
+
+            const appointmentHour = newDate.getHours();
+            if (appointmentHour < 10 || appointmentHour >= 18) {
+                res.status(400).json({ message: "Appointment time must be between 10:00 and 17:59" });
+                return;
+            }
+        }
+
+        if (observations !== undefined) {
+            appointment.observations = observations;
+        }
+
+        if (service_id) {
+            const service = await Service.findOne({ where: { id: service_id } });
+            if (!service) {
+                res.status(404).json({ message: "Service not found" });
+                return;
+            }
+            appointment.service = service;
+        }
+
+        if (user_id_client) {
+            const userClient = await User.findOne({ where: { id: user_id_client } });
+            if (!userClient) {
+                res.status(404).json({ message: "Client not found" });
+                return;
+            }
+            appointment.userClient = userClient;
+        }
+
+        if (user_id_worker) {
+            const userWorker = await User.findOne({ where: { id: user_id_worker } });
+            if (!userWorker) {
+                res.status(404).json({ message: "Worker not found" });
+                return;
+            }
+            appointment.userWorker = userWorker;
+        }
+
+        if (car_id) {
+            const car = await Car.findOne({ where: { id: car_id } });
+            if (!car) {
+                res.status(404).json({ message: "Car not found" });
+                return;
+            }
+
+            const userCar = await UserCar.findOne({ where: { user: appointment.userClient, car: car } });
+            if (!userCar) {
+                res.status(400).json({ message: "The car is not associated with the client" });
+                return;
+            }
+
+            appointment.car = car;
+        }
+
+        const getStatus = (): AppointmentStatus => {
+            const currentDate = new Date();
+            if (status && Object.values(AppointmentStatus).includes(status)) {
+                return status;
+            } else if (appointment.date < currentDate) {
+                return AppointmentStatus.COMPLETED;
+            } else if (appointment.date.toDateString() === currentDate.toDateString()) {
+                return AppointmentStatus.IN_PROGRESS;
+            } else {
+                return AppointmentStatus.PENDING;
+            }
+        };
+
+        appointment.status = getStatus();
+
+        await appointment.save(); 
+
+        console.log("Code 200: Appointment updated successfully");
+        res.status(200).json({
+            message: "Appointment updated successfully",
+            appointment: {
+                id: appointment.id,
+                date: appointment.date,
+                status: appointment.status,
+                service: appointment.service,
+                userClient: appointment.userClient,
+                userWorker: appointment.userWorker,
+                car: appointment.car,
+                observations: appointment.observations
+            }
+        });
+    } catch (error) {
+        console.error("Error updating appointment:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+},
+
+// Eliminar cita por ID
+async deleteAppointment(req: Request, res: Response): Promise<void> {
+    try {
+       const appointmentId = Number(req.params.id);
+
+        if (isNaN(appointmentId)) {
+            res.status(400).json({ message: "Invalid appointment ID" });
+            return;
+        }
+
+        const deleteResult = await Appointment.delete(appointmentId);
+        if (deleteResult.affected === 0) {
+            res.status(404).json({ message: "Appointment not found" });
+            return;
+        }
+
+        res.status(200).json({ message: `Appointment with id ${appointmentId} deleted` });
+    } catch (error) {
+        console.error('Error in deleteAppointment:', error);
+        res.status(500).json({ message: "Error deleting appointment" });
+    }
+},
+
 };
 
 
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -13,8 +13,17 @@ router.get('/worker', auth, appointmentController.getAppointmentsByWorkerId);
 // Citas de un cliente
 router.get('/client/:clientId?', auth, appointmentController.getAppointmentsByClientId);
 
+// Obtener cita por Id de cita
+router.get('/:id', auth, appointmentController.getAppointmentById);
+
 // Crear nueva cita
 router.post('/', auth, appointmentController.createAppointment);
+
+// Ruta para actualizar una cita específica
+router.put('/:id',auth, appointmentController.updateAppointment);
+
+// Eliminar una cita
+router.delete('/:id', auth, appointmentController.deleteAppointment);
 
 
 export default router;
