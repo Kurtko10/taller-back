ed87f2be44e7ebc9a09134d24cbfa1a63a0c8b8a
Kurtko10 <kurtkoaf@gmail.com>
2024-06-08 03:43:12 +0200

    Edit: correccion appointmentsSeeder, mejor relacion de usuarios y vehiculos

--- src/database/seeders/AppointmentSeeder.ts
+++ src/database/seeders/AppointmentSeeder.ts
@@ -3,7 +3,7 @@ import { Appointment } from "../../models/Appointment";
 import { AppointmentFactory } from "../factories/AppointmentFactory";
 import { SeederConfig } from "../../config/seeders";
 import { Service } from "../../models/Service";
-import { Car } from "../../models/Car";
+import { UserCar } from "../../models/UserCar";
 import { User } from "../../models/User";
 import { AppointmentStatus } from "../../constants/AppointmentStatus";
 import { getRandomValueFromArray } from "../../helpers/common";
@@ -13,26 +13,25 @@ export class AppointmentSeeder extends Seeder {
         const { APPOINTMENT } = SeederConfig;
 
         const services = await Service.find();
-        const cars = await Car.find();
-        const clients = await User.find({ where: { role: { name: 'user' } } });
+        const userCars = await UserCar.find({ relations: ["user", "car"] });
         const workers = await User.find({ where: { role: { name: 'manager' } } });
 
         const appointmentFactory = new AppointmentFactory();
         const appointments = appointmentFactory.createMany(APPOINTMENT);
 
-        const currentDate = new Date(); // Obtener la fecha actual
+        const currentDate = new Date();
 
         appointments.forEach(appointment => {
-            appointment.userClient = getRandomValueFromArray(clients);
+            const userCar = getRandomValueFromArray(userCars);
+            appointment.userClient = userCar.user;
+            appointment.car = userCar.car;
             appointment.userWorker = getRandomValueFromArray(workers);
             appointment.service = getRandomValueFromArray(services);
-            appointment.car = getRandomValueFromArray(cars);
 
-            // Comparar la fecha de la cita con la fecha actual
             if (appointment.date < currentDate) {
-                appointment.status = AppointmentStatus.COMPLETED; 
+                appointment.status = AppointmentStatus.COMPLETED;
             } else if (appointment.date.toDateString() === currentDate.toDateString()) {
-                appointment.status = AppointmentStatus.IN_PROGRESS; 
+                appointment.status = AppointmentStatus.IN_PROGRESS;
             } else {
                 appointment.status = AppointmentStatus.PENDING;
             }
